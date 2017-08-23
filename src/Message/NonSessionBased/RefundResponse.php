c41821fcd3ca09ee91650d27fc350ea3904253d9
glenn <glenn@digitickets.co.uk>
2017-08-23 11:19:24 +0100

    Add various getters to the refund response to get data out.

--- src/Message/NonSessionBased/RefundResponse.php
+++ src/Message/NonSessionBased/RefundResponse.php
@@ -3,6 +3,7 @@
 namespace DigiTickets\VerifoneWebService\Message\NonSessionBased;
 
 use DigiTickets\VerifoneWebService\Message\AbstractRemoteResponse;
+use DigiTickets\VerifoneWebService\Message\Objects\ProcessMsg\Message;
 
 class RefundResponse extends AbstractRemoteResponse
 {
@@ -23,5 +24,37 @@ class RefundResponse extends AbstractRemoteResponse
     public function getTransactionId()
     {
         return $this->data->getMsgDataAttribute('transactionid');
+    }
+
+    public function getCode()
+    {
+        // The authcode seems to be blank, but I'm including this for completeness.
+        return $this->data->getMsgDataAttribute('authcode');
+    }
+
+    public function getMessage()
+    {
+        return $this->data->getMsgDataAttribute('txnresult');
+    }
+
+    public function getTransactionReference()
+    {
+        // Build a JSON-style collection of relevant values.
+        /**
+         * @var Message $responseData
+         */
+        $responseData = $this->data;
+        return json_encode([
+            'merchantreference' => $responseData->getMsgDataAttribute('merchantreference'),
+            'transactionid' => $responseData->getMsgDataAttribute('transactionid'),
+            'tid' => $responseData->getMsgDataAttribute('tid'),
+            'schemename' => $responseData->getMsgDataAttribute('schemename'),
+            'messagenumber' => $responseData->getMsgDataAttribute('messagenumber'),
+            'pcavsresult' => $responseData->getMsgDataAttribute('pcavsresult'),
+            'ad1avsresult' => $responseData->getMsgDataAttribute('ad1avsresult'),
+            'cvcresult' => $responseData->getMsgDataAttribute('cvcresult'),
+            'arc' => $responseData->getMsgDataAttribute('arc'),
+
+        ]);
     }
 }
